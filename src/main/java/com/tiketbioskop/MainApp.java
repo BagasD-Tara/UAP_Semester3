edcb9ec0636ec3b5fe149c48a3dd02c75d945ac6
dragonlava <bagasdewantara120@gmail.com>
2025-12-21 21:47:21 +0700

    percobaan awal

--- src/main/java/com/tiketbioskop/MainApp.java
+++ src/main/java/com/tiketbioskop/MainApp.java
@@ -2,6 +2,8 @@ package com.tiketbioskop;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Tiket Bioskop!");
+        String tiket;
+
+        System.out.println("Tiket Bioskop");
     }
 }
