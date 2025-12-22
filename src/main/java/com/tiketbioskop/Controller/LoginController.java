6adb8dba920b0e597a7b6d725ed745c1737d833b
dragonlava <bagasdewantara120@gmail.com>
2025-12-23 01:40:53 +0700

    fitur login

--- src/main/java/com/tiketbioskop/Controller/LoginController.java
+++ src/main/java/com/tiketbioskop/Controller/LoginController.java
@@ -1,26 +1,52 @@
 package com.tiketbioskop.Controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
+import com.tiketbioskop.Model.User;
+import com.tiketbioskop.Utill.Session;
+import com.tiketbioskop.Utill.ExcelUtill;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 public class LoginController {
 
-    @FXML
-    private TextField usernameField;
-
-    @FXML
-    private PasswordField passwordField;
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
 
     public void handleLogin(ActionEvent event) {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        if (username.equals("admin") && password.equals("admin")) {
-            System.out.println("Login Admin berhasil");
-        } else {
-            System.out.println("Login Buyer berhasil");
+        User user = ExcelUtill.login(username, password);
+
+        if (user == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Gagal");
+            alert.setContentText("Username atau Password salah!");
+            alert.show();
+            return;
+        }
+
+        Session.setUser(user);
+
+        try {
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            FXMLLoader loader;
+
+            if (user.getRole().equals("ADMIN")) {
+                loader = new FXMLLoader(getClass().getResource("/com/tiketbioskop/view/admin_dashboard.fxml"));
+            } else {
+                loader = new FXMLLoader(getClass().getResource("/com/tiketbioskop/view/buyer_dashboard.fxml"));
+            }
+
+            stage.setScene(new Scene(loader.load()));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/com/tiketbioskop/main/MainApp.java
+++ src/main/java/com/tiketbioskop/main/MainApp.java
@@ -1,4 +1,22 @@
 package com.tiketbioskop.main;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/com/tiketbioskop/view/login.fxml"));
+
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("Sistem Pemesanan Tiket");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
